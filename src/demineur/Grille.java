48053493933249f8b15734ca37e98ceb4924f51d
joedu12 <joevin12@hotmail.fr>
2016-11-24 21:49:22 +0100

    Update Grille.java
    Commit depuis le web.. fais chier..

--- src/demineur/Grille.java
+++ src/demineur/Grille.java
@@ -1,9 +1,37 @@
 package demineur;
 
+import java.util.Scanner;
+
 public class Grille {
 
+	static int numCase = 121;
+	static int nbCoup = 0;
+	static int nbMine = 13;
+	static int choixRestant = 200;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("******************************");
+		System.out.println("\tJEU DU DÉMINEUR");
+		System.out.println("******************************\n");
+		
+		System.out.println("GRILLE | nombre : mines : " + nbMine + " - choix restants : " + choixRestant + " - nbCoups : " + nbCoup);
+		
+		for(int i=1; i<numCase; i++)
+		{
+			System.out.printf("%03d ", i);
+			if(i % 10 == 0) System.out.println(" ");
+		}
+		
+		System.out.println("\nGRILLE OUVERTE");
+		for(int i=1; i<numCase; i++)
+		{
+			System.out.print(" "+1+" ");
+			if(i % 10 == 0) System.out.println(" ");
+		}
+		
+		Scanner clavier = new Scanner(System.in);
+		System.out.println("\nChoisir une case : ");
+		int choix = clavier.nextInt();
 
 	}
 
