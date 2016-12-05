6c508ca921bb34ff355cf038056093a2efc36d46
joedu12 <joevin12@hotmail.fr>
2016-12-05 23:09:34 +0100

    La classe abstraite Point était inutile !

--- src/demineur/Case.java
+++ src/demineur/Case.java
@@ -1,36 +1,17 @@
 package demineur;
 
-class Point {
+public class Case {
 	// attributs privés
-	int x,y;
-	
-	// constructeur
-	public Point(int x, int y) { this.x = x; this.y = y;}
+	boolean bombe, decouverte, drapeau;
+	int valeur;
 	
 	// méthodes (mutateurs et assesseurs)
-	public int getX() { return x; }
-	public int getY() { return y; }
-	public void setX(int x) { this.x = x; }
-	public void setY(int y) { this.y = y; }
-}
-
-public class Case extends Point {
-	// attributs privés
-	boolean estBombe, estDecouverte, estDrapeau;
-
-	// constructeur qui hérite du constructeur parent
-	public Case(int x, int y) { super(x, y); }
-	
-	// méthodes (mutateurs et assesseurs)
-	public boolean getBombe()		{ return estBombe; }
-	public boolean getDecouverte() 	{ return estDecouverte; }
-	public boolean getDrapeau()		{ return estDrapeau; }
-	public void setBombe(boolean b)		{ estBombe = b; }
-	public void setDecouverte(boolean d){ estDecouverte = d; }
-	public void setDrapeau(boolean d)	{ estDrapeau = d; }
-	
-	
-	/* On pourrait se passer de tout les mutateurs et acesseurs si on rendait les attributs publics
-	 * ... mais c'est peut-être une mauvaise pratique -> voir avec M. Pitiot
-	 */
+	public boolean isBombe()		{ return bombe; }
+	public boolean isDecouverte() 	{ return decouverte; }
+	public boolean isDrapeau()		{ return drapeau; }
+	public int getValeur()			{ return valeur; }
+	public void setBombe(boolean b)		{ bombe = b; }
+	public void setDecouverte(boolean d){ decouverte = d; }
+	public void setDrapeau(boolean d)	{ drapeau = d; }
+	public void setValeur(int v)		{ valeur = v;}
 }
