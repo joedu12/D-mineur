d77e305d06682c8270e6a9ae85e8d29fbf4ddb12
joedu12 <joevin12@hotmail.fr>
2016-12-06 20:16:16 +0100

    Association nbCase à numMatrice et perdu/gagné

--- src/demineur/Grille.java
+++ src/demineur/Grille.java
@@ -4,37 +4,58 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Grille {
+
 	enum Difficulte {Facile, Moyen, Difficile};
-	int nbCases, nbMines, chercherX, chercherY;
+	int nbCases, nbMines, caseChoisie;
 	Difficulte difficulte;
 	Scanner clavier = new Scanner(System.in);
 	Case tableauCases[][];
+	boolean perdu = false;
 	
 	void afficher(){
 		do
 		{
 			System.out.println("\nGRILLE | " + nbMines + " mines | x choix restants | x coups restants");
-
+			
+			int z=0; // j'affiche les num√©ros de cases
+			for(int x=0; x<nbCases; x++) {
+				for(int y=0; y<nbCases; y++)
+					System.out.printf("%03d ", ++z);
+				System.out.println();
+			}
+			
+			System.out.println("\nGRILLE OUVERTE");
 			for(int x=0; x<nbCases; x++)	{
 				for(int y=0; y<nbCases; y++) {
-					if(tableauCases[x][y].isDecouverte())
-						System.out.print(" "+tableauCases[x][y].getValeur()+" ");						
+					if(tableauCases[x][y].isBombe() && perdu)
+						System.out.print(" X ");
+					else if(tableauCases[x][y].isDecouverte())
+						System.out.print(" "+tableauCases[x][y].getValeur()+" ");
 					else
-						System.out.print(" * ");
+						System.out.print(" - ");
 				}
 				System.out.println();
 			}
 			
-			System.out.print("\nChoisir une case, x : ");
-			chercherX = clavier.nextInt();
-			System.out.print("\nChoisir une case, y : ");
-			chercherY = clavier.nextInt();
-			tableauCases[chercherX][chercherY].setDecouverte(true);
-			
-		} while(chercherX != 0);
-	} 
+			System.out.print("\nChoisir une case : ");
+			caseChoisie = clavier.nextInt();
+			decouvrirCase();
+		} while(caseChoisie!=0);
+	}
 	
-	void rÈglageDifficultÈ(Difficulte diff) {
+	void decouvrirCase() {
+		int X=0, Y=0, z=0;
+		for(int x=0; x<nbCases; x++)
+			for(int y=0; y<nbCases; y++)
+			{
+				if(++z == caseChoisie) { X=x; Y=y; }
+			}
+		
+		tableauCases[X][Y].setDecouverte(true);
+		if(tableauCases[X][Y].isBombe()) perdu = true;
+	}
+	
+	void r√©glageDifficult√©(Difficulte diff) {
 		switch(diff)
 		{
 			case Facile	: nbCases = 10; nbMines = 10; break;
@@ -64,7 +85,7 @@ public class Grille {
 			}
 		}
 		
-		/* Afficher les mines */
+		/* Afficher les mines
 		 System.out.println("\nMINES : ");
 		 for(x=0; x<nbCases; x++)
 		 {
@@ -74,7 +95,7 @@ public class Grille {
 			 	else System.out.print("   ");
 			 }
 			 System.out.println(" ");
-		 }
+		 } */
 	}
 	 
 	public static void main(String[] args) {
@@ -83,7 +104,7 @@ public class Grille {
 		System.out.println("******************************");
 		
 		Grille grille = new Grille();
-		grille.rÈglageDifficultÈ(Difficulte.Facile);
+		grille.r√©glageDifficult√©(Difficulte.Facile);
 		grille.initialisation();
 		grille.afficher();
 	}
